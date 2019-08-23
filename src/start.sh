de81e156690c1a1c69c23b8249a4fb1a6263a0af
xnyan <x29yan@uwaterloo.ca>
2019-08-23 18:50:29 -0400

    updated start.sh script

--- src/start.sh
+++ src/start.sh
@@ -1,7 +1,10 @@
 
 ./master/master > m.log 2>&1 &
 
+sleep 2
+
 for i in {1..3}
 do
-  ./server/server -port 707${$i} -key key.dat -g > s${i}.log 2>&1 &
+  #./server/server -port 707${i} -key key.dat -g > s${i}.log 2>&1 &
+  ./server/server -port 707${i} -key key.dat > s${i}.log 2>&1 &
 done
